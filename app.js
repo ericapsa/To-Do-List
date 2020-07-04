bc77b54558d12cd45df42db13266651c3cb68e79
ericapsa <ericaps@umich.edu>
2020-07-04 11:11:44 -0500

    Uhhh broken

--- app.js
+++ app.js
@@ -8,22 +8,18 @@ function newItem(){
   /*Store the list element in a var*/
   var list = document.getElementById("list");
 
-  var oldList = localStorage.getItem(list);
-  if (oldList != null){
-    list.appendChild(document.createTextNode(oldList));
-  }
+  // Append all old list items from local storage
+  getLocally();
 
-
+  // create a new bullet element
   var el = document.createElement("li");
 
-  /* access the li var. append a new:
-      create a "textnode" that has a - then the item var we stored earlier*/
+  /* access the bullet element var, append a new:
+      create a "textnode" that has a bullet then the item var we stored earlier*/
   el.appendChild(document.createTextNode("☀ " + item));
 
   /*add it to the list!*/
   list.appendChild(el);
-  /*Save in local storage*/
-  localStorage.setItem(list);
 
   /*Clear the text in the box*/
   document.getElementById("input").value = " ";
@@ -31,14 +27,30 @@ function newItem(){
   /*Remove item when the li element is clicked*/
   el.onclick = removeItem;
 
+  /*Save in local storage*/
+  saveLocally(el);
 
 }
 
+function getLocally(list) {
+  console.log("retrieving old data");
+  var oldList = localStorage.getItem(list);
+  if (oldList != null){
+    list.appendChild(document.createTextNode(oldList));
+  }
+}
+
+function saveLocally(el){
+  console.log("saving new data");
+  // Get element
+  // slap it in local data
+  localStorage.setItem(el); // ??
+}
 
 function removeItem(e) {
   // when the funcition is called on an item (e) it will get stored in e.target, then is remove() d
-  // e.target.remove();
-  localStorage.removeItem(e);
+  e.target.remove();
+  localStorage.removeItem(e); // ??
 }
 
 document.body.onkeyup = function(e) {
